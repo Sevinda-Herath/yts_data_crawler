6d65db1e5743937bdaafd701ea4068861f128289
Sevinda Herath <119860637+Sevinda-Herath@users.noreply.github.com>
2024-11-20 12:34:25 +0530

    Update yts_data_crawler.py

--- yts_data_crawler/spiders/yts_data_crawler.py
+++ yts_data_crawler/spiders/yts_data_crawler.py
@@ -17,16 +17,18 @@ class CrawlingSpider(CrawlSpider):
 
         yield {
             'URL': response.url,
-            'Movie_Title': response.css('.hidden-xs h1::text').get(),
-            'Released_Year': response.css('.hidden-xs h2::text').getall()[0],
+            'MovieTitle': response.css('.hidden-xs h1::text').get(),
+            'ReleasedYear': response.css('.hidden-xs h2::text').getall()[0],
+            'MoviePoster': response.css('.row #movie-poster::text').getall()[0],
             'Genres': response.css('.hidden-xs h2::text').getall()[1],
-            'YTS-Likes': response.css('.rating-row #movie-likes::text').get(),
-            'IMDb-Rating': response.css('.rating-row [itemprop="ratingValue"]::text').get(),
-            'IMDb-Votes': response.css('.rating-row [itemprop="ratingCount"]::text').get(),
-            'Movie_Director': response.css('.list-cast-info [itemprop="director"] [itemprop="name"]::text').getall(),
+            'MoviePosterSrc': response.css('.row #movie-poster img::attr(src)').get(),
+            'YTSLikes': response.css('.rating-row #movie-likes::text').get(),
+            'IMDbRating': response.css('.rating-row [itemprop="ratingValue"]::text').get(),
+            'IMDbVotes': response.css('.rating-row [itemprop="ratingCount"]::text').get(),
+            'MovieDirector': response.css('.list-cast-info [itemprop="director"] [itemprop="name"]::text').getall(),
             'Runtime': runtime,
             'Seeds': response.css('div.tech-spec-element.col-xs-20.col-sm-10.col-md-5 font::text').get(),
             'Uploader': response.css('#synopsis a::text').get(),
-            'Uploaded-Time': response.css('#synopsis span em::text').get(),
+            'UploadedTime': response.css('#synopsis span em::text').get(),
         }
         
